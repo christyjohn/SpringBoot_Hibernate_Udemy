ddec158c4a7ebdfde724f5f1d1e48b6d5fdd2d77
Christy John <christyjohn.crz@gmail.com>
2026-01-24 14:57:49 +0530

    Modified the sql scripts for the project

---
+++ 03-spring-boot-hibernate-jpa-crud/00-starter-sql-scripts/02-products-tracker.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE  IF NOT EXISTS `products_tracker`;
+USE `products_tracker`;
+
+--
+-- Table structure for table `products`
+--
+
+DROP TABLE IF EXISTS `products`;
+
+CREATE TABLE `products` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `product_name`varchar(45) DEFAULT NULL,
+  `sku` VARCHAR(100) DEFAULT NULL,
+  `price` DECIMAL(10, 2) DEFAULT NULL,
+  `product_description` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+
