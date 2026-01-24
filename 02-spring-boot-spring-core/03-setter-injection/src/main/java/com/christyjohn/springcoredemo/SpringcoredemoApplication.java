ca5b8cd4382bddb740cf5c4f025318ef4d3e8130
Christy John <christyjohn.crz@gmail.com>
2026-01-24 13:45:38 +0530

    Removing unnecessary component scanning code

--- 02-spring-boot-spring-core/03-setter-injection/src/main/java/com/christyjohn/springcoredemo/SpringcoredemoApplication.java
+++ 02-spring-boot-spring-core/03-setter-injection/src/main/java/com/christyjohn/springcoredemo/SpringcoredemoApplication.java
@@ -3,10 +3,7 @@ package com.christyjohn.springcoredemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(
-		scanBasePackages = { "com.christyjohn.springcoredemo",
-				             "com.christyjohn.util"}
-)
+@SpringBootApplication
 public class SpringcoredemoApplication {
 
 	public static void main(String[] args) {
