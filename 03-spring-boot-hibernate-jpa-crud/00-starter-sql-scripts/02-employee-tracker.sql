bc4b4aed8ac893105dc84bdcc736325145948894
Christy John <christyjohn.crz@gmail.com>
2026-01-24 14:51:57 +0530

    Added the sql scripts for the project

--- 03-spring-boot-hibernate-jpa-crud/00-starter-sql-scripts/02-employee-tracker.sql
+++ 03-spring-boot-hibernate-jpa-crud/00-starter-sql-scripts/02-employee-tracker.sql
@@ -1,17 +1,18 @@
-CREATE DATABASE  IF NOT EXISTS `student_tracker`;
-USE `student_tracker`;
+CREATE DATABASE  IF NOT EXISTS `employee_tracker`;
+USE `employee_tracker`;
 
 --
--- Table structure for table `student`
+-- Table structure for table `employee`
 --
 
-DROP TABLE IF EXISTS `student`;
+DROP TABLE IF EXISTS `employee`;
 
-CREATE TABLE `student` (
+CREATE TABLE `employee` (
   `id` int NOT NULL AUTO_INCREMENT,
   `first_name`varchar(45) DEFAULT NULL,
   `last_name` varchar(45) DEFAULT NULL,
   `email` varchar(45) DEFAULT NULL,
+  `job_description` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 
