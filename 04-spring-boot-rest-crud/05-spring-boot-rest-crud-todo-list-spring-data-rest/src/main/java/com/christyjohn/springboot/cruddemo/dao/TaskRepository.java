0b140b35cb0d1ed6248d7cec38f86083e2602409
Christy John <christyjohn.crz@gmail.com>
2026-01-27 18:37:00 +0530

    Adding @RepositoryRestResource to change the resource name, and add page-size

--- 04-spring-boot-rest-crud/05-spring-boot-rest-crud-todo-list-spring-data-rest/src/main/java/com/christyjohn/springboot/cruddemo/dao/TaskRepository.java
+++ 04-spring-boot-rest-crud/05-spring-boot-rest-crud-todo-list-spring-data-rest/src/main/java/com/christyjohn/springboot/cruddemo/dao/TaskRepository.java
@@ -3,6 +3,7 @@ package com.christyjohn.springboot.cruddemo.dao;
 import com.christyjohn.springboot.cruddemo.entity.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+//@RepositoryRestResource(path="todolist")
 public interface TaskRepository extends JpaRepository<Task, Integer> {
 
     // that's it ... no need to write any code LOL!
