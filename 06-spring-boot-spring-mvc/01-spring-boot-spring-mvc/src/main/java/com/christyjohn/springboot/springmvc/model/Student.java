a976ddd0f80f3c156926747d3495f3ebaae3d604
Christy John <christyjohn.crz@gmail.com>
2026-01-28 15:29:02 +0530

    Refactored the folder name

---
+++ 06-spring-boot-spring-mvc/01-spring-boot-spring-mvc/src/main/java/com/christyjohn/springboot/springmvc/model/Student.java
@@ -0,0 +1,20 @@
+package com.christyjohn.springboot.springmvc.model;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@Getter
+@Setter
+public class Student {
+    private String firstName;
+    private String lastName;
+    private String country;
+    private String favoriteLanguage;
+    private List<String> favoriteSystems;
+}
