33bb97ccd87ccc6df8418668ba2218b9c9a4620c
Christy John <christyjohn.crz@gmail.com>
2026-01-28 11:30:26 +0530

    Spring Boot security when using cutom tables

---
+++ 05-spring-boot-rest-security/06-spring-boot-rest-security-jdbc-bcrypt-custom-table-names/src/main/java/com/christyjohn/springboot/cruddemo/exceptions/TaskNotFoundException.java
@@ -0,0 +1,16 @@
+package com.christyjohn.springboot.cruddemo.exceptions;
+
+public class TaskNotFoundException extends RuntimeException {
+
+    public TaskNotFoundException(String message) {
+        super(message);
+    }
+
+    public TaskNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public TaskNotFoundException(Throwable cause) {
+        super(cause);
+    }
+}
---
+++ 05-spring-boot-rest-security/06-spring-boot-rest-security-jdbc-bcrypt-custom-table-names/src/main/java/com/christyjohn/springboot/cruddemo/security/DemoSecurityConfig.java
@@ -0,0 +1,53 @@
+package com.christyjohn.springboot.cruddemo.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DemoSecurityConfig {
+
+    // add support for JDBC ... no more hardcoded users :-)
+    @Bean
+    public UserDetailsManager userDetailsManager(DataSource dataSource) {
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+
+        // define query to retrieve a user by username
+        jdbcUserDetailsManager.setUsersByUsernameQuery(
+                "select user_id, pw, active from members where user_id=?");
+
+        // define query to retrieve the authorities/roles by username
+        jdbcUserDetailsManager.setAuthoritiesByUsernameQuery(
+                "select user_id, role from roles where user_id=?");
+
+        return jdbcUserDetailsManager;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(configurer ->
+                configurer
+                        .requestMatchers(HttpMethod.GET, "/api/tasklist").hasRole("EMPLOYEE")
+			            .requestMatchers(HttpMethod.GET, "/api/tasklist/**").hasRole("EMPLOYEE")
+			            .requestMatchers(HttpMethod.POST, "/api/tasklist").hasRole("MANAGER")
+			            .requestMatchers(HttpMethod.PUT, "/api/tasklist").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.PATCH, "/api/tasklist/**").hasRole("MANAGER")
+			            .requestMatchers(HttpMethod.DELETE, "/api/tasklist/**").hasRole("ADMIN")
+	    );
+
+        // use Http Basic authentication
+        http.httpBasic(Customizer.withDefaults());
+
+        // disable Cross Site Request Forgery (CSRF)
+        http.csrf(csrf -> csrf.disable());
+
+        return http.build();
+    }
+}
