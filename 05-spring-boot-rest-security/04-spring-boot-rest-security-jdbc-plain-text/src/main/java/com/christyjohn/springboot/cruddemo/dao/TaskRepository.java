d029ef4cd30931a3cdb672f327aa69d8e0e8faef
Christy John <christyjohn.crz@gmail.com>
2026-01-28 08:00:48 +0530

    cleanup

--- 05-spring-boot-rest-security/04-spring-boot-rest-security-jdbc-plain-text/src/main/java/com/christyjohn/springboot/cruddemo/dao/TaskRepository.java
+++
@@ -1,9 +0,0 @@
-package com.christyjohn.springboot.cruddemo.dao;
-
-import com.christyjohn.springboot.cruddemo.entity.Task;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TaskRepository extends JpaRepository<Task, Integer> {
-
-    // that's it ... no need to write any code LOL!
-}
