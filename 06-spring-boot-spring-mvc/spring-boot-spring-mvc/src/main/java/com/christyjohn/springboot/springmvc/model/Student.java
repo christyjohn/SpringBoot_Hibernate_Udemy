97eabd31650e1218a5967cdd7b73b89168edfa08
Christy John <christyjohn.crz@gmail.com>
2026-01-28 14:22:45 +0530

    Form Data Binding - Text Fields

---
+++ 06-spring-boot-spring-mvc/spring-boot-spring-mvc/src/main/java/com/christyjohn/springboot/springmvc/model/Student.java
@@ -0,0 +1,15 @@
+package com.christyjohn.springboot.springmvc.model;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Data
+@NoArgsConstructor
+@Getter
+@Setter
+public class Student {
+    private String firstName;
+    private String lastName;
+}
