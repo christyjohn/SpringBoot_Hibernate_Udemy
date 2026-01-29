7236de19ac10e55df6fd843c360a5c0d3b0d9286
Christy John <christyjohn.crz@gmail.com>
2026-01-29 12:42:47 +0530

    Get products basic impl

---
+++ 07-spring-boot-spring-mvc-crud/spring-boot-spring-mvc-crud/src/main/java/com/luv2code/springboot/cruddemo/controller/ProductController.java
@@ -0,0 +1,33 @@
+package com.luv2code.springboot.cruddemo.controller;
+
+import com.luv2code.springboot.cruddemo.entity.Product;
+import com.luv2code.springboot.cruddemo.service.ProductService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/products")
+public class ProductController {
+
+    private ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    // add  mapping for "/list"
+    @GetMapping("/list")
+    public String listProducts(Model model) {
+        // get the products from db
+        List<Product> products = productService.findAll();
+
+        // add that to the spring model
+        model.addAttribute("products", products);
+
+        return "list-products";
+    }
+}
