bc5daf43cf6d88047000baff40aab8d77c7ce1ec
Christy John <christyjohn.crz@gmail.com>
2026-01-29 09:59:38 +0530

    Added regex validation for postal code

--- 06-spring-boot-spring-mvc/02-spring-boot-spring-mvc-validation/src/main/java/com/christyjohn/springboot/mvc/entity/Customer.java
+++ 06-spring-boot-spring-mvc/02-spring-boot-spring-mvc-validation/src/main/java/com/christyjohn/springboot/mvc/entity/Customer.java
@@ -1,9 +1,6 @@
 package com.christyjohn.springboot.mvc.entity;
 
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -22,4 +19,7 @@ public class Customer {
     @Min(value=0, message="must be greater than or equal to zero")
     @Max(value=10, message="must be less than or equal to ten")
     private int freePasses;
+
+    @Pattern(regexp = "^[a-zA-Z0-9]{5}", message="only 5 chars/digits")
+    private String postalCode;
 }
