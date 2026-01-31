4c480cea05ed06bc8a030b515089527805dbf5a1
Christy John <christyjohn.crz@gmail.com>
2026-01-31 09:40:02 +0530

    Initialize the project

---
+++ 10-spring-boot-aop/01-spring-boot-aop-before-advice/src/test/java/com/christyjohn/aopdemo/AopdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.christyjohn.aopdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AopdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
