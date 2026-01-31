26342bc77f762b1a0b71482a2a8d376c1905b5d7
Christy John <christyjohn.crz@gmail.com>
2026-01-31 18:29:09 +0530

    Refactoring

--- 11-whats-new-in-spring-boot-4/spring-boot-api-versioning-helloworld/src/main/java/com/luv2code/apiversioning/demo/rest/HelloWorldRestController.java
+++ 11-whats-new-in-spring-boot-4/spring-boot-api-versioning-helloworld/src/main/java/com/luv2code/apiversioning/demo/rest/HelloWorldRestController.java
@@ -1,22 +1,24 @@
 package com.luv2code.apiversioning.demo.rest;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/{version}/hello")
 public class HelloWorldRestController {
 
-    @GetMapping(path="/api/{version}/hello", version="1")
+    @GetMapping(version="1")
     public String helloV1() {
         return "Hello World from API v1";
     }
 
-    @GetMapping(path="/api/{version}/hello", version="2")
+    @GetMapping(version="2")
     public String helloV2() {
         return "Howdy World from API v2";
     }
 
-    @GetMapping(path="/api/{version}/hello", version="3")
+    @GetMapping(version="3")
     public String helloV3() {
         return "Hey Hey World from API v3";
     }
