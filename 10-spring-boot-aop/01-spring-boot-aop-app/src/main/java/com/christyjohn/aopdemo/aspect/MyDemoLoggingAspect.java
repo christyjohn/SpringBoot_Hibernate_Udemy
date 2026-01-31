ba582a34e5161dc426b545366639bb40b347d595
Christy John <christyjohn.crz@gmail.com>
2026-01-31 12:43:47 +0530

    combining pointcut declaration; not for getter / setter

--- 10-spring-boot-aop/01-spring-boot-aop-app/src/main/java/com/christyjohn/aopdemo/aspect/MyDemoLoggingAspect.java
+++ 10-spring-boot-aop/01-spring-boot-aop-app/src/main/java/com/christyjohn/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -14,7 +14,19 @@ public class MyDemoLoggingAspect {
     @Pointcut("execution(* com.christyjohn.aopdemo.dao.*.*(..))")
     private void forDaoPackage() {}
 
-    @Before("forDaoPackage()")
+    // create a pointcut for getter methods
+    @Pointcut("execution(* com.christyjohn.aopdemo.dao.*.get*(..))")
+    private void getter() {}
+
+    // create a pointcut for setter methods
+    @Pointcut("execution(* com.christyjohn.aopdemo.dao.*.set*(..))")
+    private void setter() {}
+
+    // create pointcut: include package ... exclude getter/setter
+    @Pointcut("forDaoPackage() && !(getter() || setter())")
+    private void forDaoPackageNoGetterSetter() {}
+
+    @Before("forDaoPackageNoGetterSetter()")
     public void beforeAddAccountAdvice() {
         System.out.println("\n=====>>> Executing @Before advice on method");
     }
