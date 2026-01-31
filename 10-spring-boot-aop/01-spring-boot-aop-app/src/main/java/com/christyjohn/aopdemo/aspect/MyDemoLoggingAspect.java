c66508e388f4aeee500bf11cb18a24d23eceb5b0
Christy John <christyjohn.crz@gmail.com>
2026-01-31 11:22:42 +0530

    apply pointcut expr on any AccountDAO addAccount() only

--- 10-spring-boot-aop/01-spring-boot-aop-app/src/main/java/com/christyjohn/aopdemo/aspect/MyDemoLoggingAspect.java
+++ 10-spring-boot-aop/01-spring-boot-aop-app/src/main/java/com/christyjohn/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -9,7 +9,9 @@ import org.springframework.stereotype.Component;
 public class MyDemoLoggingAspect {
     // this is where we add all of our related advices for logging
     // let's start with an @Before advice
-    @Before("execution(public void addAccount())")
+
+    //@Before("execution(public void addAccount())")
+    @Before("execution(public void com.christyjohn.aopdemo.dao.AccountDAO.addAccount())")
     public void beforeAddAccountAdvice() {
         System.out.println("=====>>> Executing @Before advice on addAccount()");
     }
